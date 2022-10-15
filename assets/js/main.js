bb5589f5c3053ad3ade09d609b7e330ee667bc51
Gus Ruben <gusruben@protonmail.com>
2022-10-14 23:57:38 -0400

    Add Form Validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,9 +83,42 @@ window.onload = async () => {
 // - anhatthezoo 
 
 function addItemFormOnSubmit() {
-    let file = document.querySelector("#add-item-form-image").files[0],
+
+    // clear old invalidation (if any)
+    clearInvalidation(document.getElementById("add-item-form-name"));
+    clearInvalidation(document.getElementById("add-item-form-quantity"));
+    clearInvalidation(document.getElementById("add-item-form-image"));
+
+    let invalid = false;
+
+    // empty input validation
+    if (!document.getElementById("add-item-form-name").value) {
+        invalidateInput(document.getElementById("add-item-form-name"), "You need to enter an item name!");
+        invalid = true;
+    }
+    if (!document.getElementById("add-item-form-quantity").value) {
+        invalidateInput(document.getElementById("add-item-form-quantity"), "You need to enter a quantity!");
+        invalid = true;
+    } else if (isNaN(document.getElementById("add-item-form-quantity").value)) {
+        // number validation
+        invalidateInput(document.getElementById("add-item-form-quantity"), "That's not a number!");
+        invalid = true;
+    }
+
+    let file = document.querySelector("#add-item-form-image").files[0];
+    let reader;
+    
+    if (file) {
         reader = new FileReader();
-    if (file) { reader.readAsDataURL(file); }
+        reader.readAsDataURL(file);
+    } else {
+        // if no file is uploaded, error out and don't submit form
+        document.getElementById("add-item-form-image").parentElement.parentElement.classList.add("is-danger");
+        invalid = true;
+    }
+
+    if (invalid) { return; }
+
     // Add item to database after FileReader has finished     
     reader.addEventListener("load", async () => {
         await addNewItemToDatabase(
@@ -149,6 +182,8 @@ for (let elem of document.getElementsByTagName("th")) {
 
 // event listener for form file upload
 document.getElementById("add-item-form-image").addEventListener("change", e => {
+    if (!e.target.files) { return; }
+
     // show filename next to button
     let span = document.createElement("span");
     span.className = "file-name";
@@ -156,4 +191,66 @@ document.getElementById("add-item-form-image").addEventListener("change", e => {
     e.target.parentElement.appendChild(span);
 
     e.target.parentElement.parentElement.classList.add("has-name");
-})
+    
+    // clear invalidation (if it was invalid before)
+    clearInvalidation(e.target);
+});
+
+// event listeners for form text inputs
+document.getElementById("add-item-form-name").addEventListener("blur", e => {
+    clearInvalidation(e.target);
+    if (e.target.value) {
+        clearInvalidation(e.target);
+    } else {
+        invalidateInput(e.target, "You need to enter an item name!");
+    }
+});
+document.getElementById("add-item-form-quantity").addEventListener("blur", e => {
+    clearInvalidation(e.target);
+    if (!e.target.value) {
+        invalidateInput(e.target, "You need to enter a quantity!");
+    } else if (isNaN(e.target.value)) {
+        invalidateInput(e.target, "That's not a number!");
+    } else {
+        clearInvalidation(e.target);
+    }
+});
+
+// helper method for changing css and adding text for invalid inputs
+function invalidateInput(input, warningText) {
+
+    input.classList.add("is-danger");
+
+    // warning text
+    let p = document.createElement("p");
+    p.className = "help is-danger";
+    p.innerText = warningText;
+    input.parentElement.parentElement.appendChild(p);
+
+    // warning icon
+    input.parentElement.classList.add("has-icons-right");
+    let span = document.createElement("span");
+    span.className = "icon is-small is-right";
+    let i = document.createElement("i");
+    i.className = "fas fa-exclamation-triangle";
+    span.appendChild(i);
+
+    input.parentElement.appendChild(span);
+
+}
+
+// remove invalidation
+function clearInvalidation(input) {
+
+    input.classList.remove("is-danger");
+    if (input.type == "file") {
+        input.parentElement.parentElement.classList.remove("is-danger");
+    }
+
+    let warningText = input.parentElement.parentElement.querySelector(".help.is-danger");
+    if (warningText) { warningText.remove(); }
+
+    let icon = input.parentElement.querySelector(".icon.is-small.is-right");
+    if (icon) { icon.remove(); }
+
+}
