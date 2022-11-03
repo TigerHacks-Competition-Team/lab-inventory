51c98875d7d0529c9a65acae6e9ad3dc6c551172
Gus Ruben <gusruben@protonmail.com>
2022-11-02 22:37:07 -0400

    Move to Sass

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ An inventory website, created for the Jackson Reed robotics lab.
 
 Bulma is a CSS framework, in some ways similar to Boostrap, intended to make it easier to build the UI. The docs can be found at [bulma.io/documentation](https://bulma.io/documentation/). Bulma has no JavaScript dependencies.
 
+### [Sass](https://sass-lang.com/)
+
+Sass is a 'CSS preprocessor.' This basically means that you can write modified pseudo-CSS with additional features, and sass will compile it into normal, minfied CSS. Some notable features it includes are nested selectors, functions, and mixins. The docs are at [sass-lang.com/documentation](https://sass-lang.com/documentation/).
+
+To be able to compile with Sass, you need to have it installed on your system. You can install it from npm with `npm install -g sass`. If you use VSCode, there are two included build scripts which you can run with `Ctrl+Shift+B`: `Compile Sass` and `Watch Sass`. `Compile Sass` compiles the `.scss` files once, and `Watch Sass` runs the same command but with `--watch`, so sass will watch for changes and compile as you code. These scripts can be found in [`.vscode/tasks.json`](.vscode/tasks.json)
+
+If you don't use VSCode, you can just copy the commands from `tasks.json` and run them in your terminal, or set up your own build system.
+
 ### [Sortable](https://github.com/HubSpot/sortable)
 
 Sortable is a tiny (`<2kb`) drop-in script for creating sortable tables. It doesn't have much of an API, but for what there is, there are docs at [github.hubspot.com/sortable](https://github.hubspot.com/sortable/api/options/).
--- assets/js/styling.js
+++ assets/js/styling.js
@@ -2,39 +2,28 @@
 // dark mode, load before other things so page doesn't flash white
 let theme = window.localStorage.getItem("theme") || 
     (window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light");
-if (theme == "dark") {
-    enableDarkTheme()
+setTheme(theme);
+
+function setTheme(theme) {
+    let style = document.getElementById("theme");
+    style.href = `assets/css/${theme}/${theme}.css`;
+    style.dataset.theme = theme;
+
+    document.getElementById("theme-icon").className = "fas";
+    document.getElementById("theme-icon").classList.add(
+                                    theme == "light" ? "fa-sun" : "fa-moon");
 }
 
-function enableDarkTheme() {
-    let link = document.createElement("link");
-    link.id = "dark-theme";
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "https://unpkg.com/bulmaswatch/darkly/bulmaswatch.min.css";
-
-    let head = document.getElementsByTagName("head")[0];
-    head.insertBefore(link, document.getElementById("override-styles"));
-
-    document.getElementById("theme-icon").classList.remove("fa-sun")
-    document.getElementById("theme-icon").classList.add("fa-moon")
-
-    document.body.className = "dark";
-}
-
-// dark mode button
+// theme changer button
 document.getElementById("theme-button").addEventListener("click", e => {
+    let style = document.getElementById("theme");
 
-    if (document.getElementById("dark-theme")) {
+    if (style.dataset.theme == "dark") {
 
-        document.getElementById("dark-theme").remove();
-        document.body.classList.remove("dark");
-        document.getElementById("theme-icon").classList.remove("fa-moon");
-        document.getElementById("theme-icon").classList.add("fa-sun");
-
-        window.localStorage.setItem("theme", "light")
+        window.localStorage.setItem("theme", "light");
+        setTheme("light");
     } else {
-        window.localStorage.setItem("theme", "dark")
-        enableDarkTheme();
+        window.localStorage.setItem("theme", "dark");
+        setTheme("dark");
     }
 });
